a03f40eb4741cc487fa8fed1a403349aaf5b4cc3
QB1 <QB1@QB1>
2020-04-30 16:08:35 +0900

    아름) 04/30 love, hate insert 일어날 때 board 카운트 동시 적용 트랜잭션 처리

--- src/main/java/com/aeho/demo/service/HateServiceImpl.java
+++ src/main/java/com/aeho/demo/service/HateServiceImpl.java
@@ -2,7 +2,9 @@ package com.aeho.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.aeho.demo.dao.BoardDao;
 import com.aeho.demo.dao.HateDao;
 import com.aeho.demo.vo.HateVo;
 
@@ -11,11 +13,27 @@ public class HateServiceImpl implements HateService {
 	
 	@Autowired
 	private HateDao hateDao;
+	
+	@Autowired
+	private BoardDao boardDao;
 
 	@Override
+	@Transactional(rollbackFor = Exception.class)
 	public int insertHate(HateVo hv) {
-		int re = hateDao.insertHate(hv);
-		return re;
+		int result = 0;
+		String cntkeyword = "hate";
+		//hate 증가시 board에서 lovecnt 동시 증가 트랜잭션
+		try {
+			int result_hate = hateDao.insertHate(hv);
+			int result_board = boardDao.updateCnt(hv.getB_no(), cntkeyword);
+			
+			if( result_board > 0 && result_hate > 0) {
+				result = 1;
+			}
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return result;
 	}
 
 	@Override
--- src/main/java/com/aeho/demo/service/LoveServiceImpl.java
+++ src/main/java/com/aeho/demo/service/LoveServiceImpl.java
@@ -2,7 +2,9 @@ package com.aeho.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.aeho.demo.dao.BoardDao;
 import com.aeho.demo.dao.LoveDao;
 import com.aeho.demo.vo.LoveVo;
 
@@ -12,10 +14,26 @@ public class LoveServiceImpl implements LoveService {
 	@Autowired
 	private LoveDao loveDao;
 	
+	@Autowired
+	private BoardDao boardDao;
+	
 	@Override
+	@Transactional(rollbackFor=Exception.class)
 	public int insertLove(LoveVo lv) {
-		int re = loveDao.insertLove(lv);
-		return re;
+		int result = 0;
+		String cntkeyword = "love";
+		//love 증가시 board에서 lovecnt 동시 증가 트랜잭션
+		try {
+			int result_love = loveDao.insertLove(lv);
+			int result_board = boardDao.updateCnt(lv.getB_no(), cntkeyword);
+			
+			if( result_board > 0 && result_love > 0) {
+				result = 1;
+			}
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return result;
 	}
 
 	@Override
