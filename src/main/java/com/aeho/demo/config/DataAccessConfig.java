85ac19caa67faa73e401c90f66b56a106f64d6d0
Asencding <ascending77@naver.com>
2020-05-21 17:22:35 +0900

    진탁) 커밋아되라...

--- src/main/java/com/aeho/demo/config/DataAccessConfig.java
+++ src/main/java/com/aeho/demo/config/DataAccessConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @MapperScan(basePackages = "com.aeho.demo.dao")
 public class DataAccessConfig {
+	
 	@Bean
 	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
 		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
